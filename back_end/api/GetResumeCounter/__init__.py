11198e793e59a0b06ddf09fc55e33f26b6e46faf
alex.maccalman <alex.maccalman@gmail.com>
2022-08-29 13:07:36 -0400

    backend test 4

--- back_end/api/GetResumeCounter/__init__.py
+++ back_end/api/GetResumeCounter/__init__.py
@@ -5,7 +5,7 @@ import logging
 
 import azure.functions as func
 
-# test 3
+# test 4
 
 def main(req: func.HttpRequest, readdb: func.DocumentList, updatedb: func.Out[func.Document]) -> func.HttpResponse:
     # logging.info('Python HTTP trigger function processed a request.')
